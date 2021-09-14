17944b2f2cf7d5f7adb156a5fcada8137e41ae85
BenFrancka <benfrancka@gmail.com>
2021-09-14 16:00:14 -0700

    installs dependencies, sets up routes in App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import RAndMDetailContainer from '../../containers/RAndMDetailContainer';
+import RAndMListContainer from '../../containers/RAndMListContainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:id">
+          <RAndMDetailContainer />
+        </Route>
+        <Route path="/">
+          <RAndMListContainer />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
